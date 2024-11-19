73b2dd7c5908ef2ffb9fc7e03e8593beb7c61f5f
yossi.shirizli <yossi.shirizli@live.com>
2024-11-19 21:09:29 +0200

    Baseic app

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Yossi Shirizli on 18/11/2024.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,8 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Yossi Shirizli on 18/11/2024.
+//
+
+import Foundation
